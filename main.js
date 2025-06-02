d569f1ad1607abeb09b1acdb8ac411cf95eaf917
wsderftyu <br3ndanchew@gmail.com>
2025-06-01 22:03:34 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ fishBar = document.getElementById("fishBar")
 blackBox = document.getElementById("blackBox")
 fishingHook = document.getElementById("fishingHook")
 messageText = document.getElementById("messageText")
+totalFishText = document.getElementById("totalFish")
+totalFish2Text = document.getElementById("totalFish2")
+totalTimeDisplay = document.getElementById("time")
+currentYearDisplay = document.getElementById("currentYear")
+totalTime = 61
 fishBarDirection = 1
 fishBarSpeed = 3-2*Math.random()
 fishDistFromLeft = 0
@@ -15,6 +20,18 @@ space = 0
 fishingStatus = 0
 timer = 0
 timer2 = 0
+timer3 = 0
+totalFish = 0
+totalFish2 = 0
+totalBites = 0
+totalBites2 = 0
+avgTimeList = []
+avgTimeList2 = []
+jimmy.src = 'castRod.gif'
+jimmy.src = 'idleJimmy.gif'
+currentYear = 1980
+hasSwitchedYear = 0
+reset()
 
 /*
 fishingStatus = 0: rod out of water, press space to cast
@@ -34,10 +51,12 @@ function setGif(newSrc) {
     }
 }
 
+
 function createMessage(input, whichColor){
     messageText.style.color = (whichColor)
-    messageText.style.fontSize = 25
-    messageText.textContent = input
+    messageText.style.fontSize = '30px'
+    messageText.style.textAlign = 'center'
+    messageText.innerHTML = input
 }
 
 document.addEventListener('keydown', function(event){
@@ -119,32 +138,61 @@ setInterval(function() {
 
     timer -= 0.01
     timer2 -= 0.01
+    if (fishingStatus === 3){
+        timer3 += 0.01
+    }
+
+    if (totalTime > 0.01) {
+        totalTime -=0.01
+    }
 
     if (fishingStatus === 0) {
         setGif('idleJimmy.gif')
-        createMessage("Press space to cast", "Black")
+        if (timer <= 0.1){
+            createMessage("Press space to cast", "Black")
+        }
         if (space === 1) {
             fishingStatus = 1
         }
 
     } else if (fishingStatus === 1) {
-        if (timer <= 0.1) {
-            setGif('castRod.gif')
-            timer = 2
-        }
-        fishingStatus = 2
+        setGif('test.gif')
 
-        console.log("hello")
+        if (timer <= 0.1) {
+            timer = 1.2
+            fishingStatus = 2
+        }
+ 
+
         
     } else if (fishingStatus === 2) {
+ 
         if (timer <= 0.1) {
             setGif('boredJimmy.gif')
         }
-
-        if (Math.random() > 0.999 && timer <= 0.1) {
+        if (currentYear === 1980){
+            if (Math.random() > 0.998 && timer <= 0.1) {
             createMessage("!!!", 'yellow')
             timer2 = 3
-        } 
+            totalBites += 1
+            avgTimeList.push(Math.floor(timer3))
+            timer3 = 0
+            }
+        }
+        
+        if (currentYear === 2020) {
+            if (Math.random() > 0.9998 && timer <= 0.1) {
+            createMessage("!!!", 'yellow')
+            timer2 = 3
+            totalBites2 += 1
+            avgTimeList2.push(Math.floor(timer3))
+            timer3 = 0
+            } 
+        }
+
+        if (timer2 <= 0.1) {
+            createMessage("", "")
+        }
 
         if (space === 1 && timer2 <= 0.1 && timer <= 0.1) {
             fishingStatus = 5
@@ -157,7 +205,9 @@ setInterval(function() {
     }
     
     } else if (fishingStatus === 3) {
+        jimmy.style.display = 'none'
         setGif('fishBit.gif')
+        jimmy.style.display = 'inline-block'
         if (greenPosition > 99) {
             createMessage("You got a fish!", 'green')
             fishingStatus = 4
@@ -168,27 +218,77 @@ setInterval(function() {
             createMessage("Fail...", 'red')
         }
     } else if (fishingStatus === 4) {
+
         setGif('gotAFish.gif')
+        
+            if (currentYear === 1980){
+                totalFish += 1
+            }
+            if (currentYear === 2020) {
+                totalFish2 += 1
+            }
         if (timer <= 0.1) {
             fishingStatus = 0
             reset()
         }
     } else if (fishingStatus === 5) {
+        timer3 = 0
         setGif('noFish.gif')
         if (timer <= 0.1) {
             fishingStatus = 0
             reset()
+
         }
     }
 
-    
+    if (totalTime < 1) {
+        if (currentYear === 1980) {
+            createMessage(`You caught ${totalFish} fish`, "Black")
+            reset()
+            timer = 2
+            if (fishingStatus === 2 || fishingStatus === 3) {
+                fishingStatus = 5
+            }
+            currentYear = 2020
+        }
+
+        if (currentYear === 2020  && hasSwitchedYear === 1) {
+            fishingStatus = 1500
+
+            function calculateAverage(list) {
+                if (list.length > 0) {
+                    const sum = list.reduce((acc, curr) => acc + curr, 0);
+                    return sum / list.length;
+                } else {
+                    return "N/A"
+                }
+
+            }
+
+            avgTime = calculateAverage(avgTimeList);
+            avgTime2 = calculateAverage(avgTimeList2);
+            createMessage(`In 1980, you got ${totalBites} bites and caught ${totalFish} fish. In 2020, you got ${totalBites2} bites and caught ${totalFish2} fish. Your average wait time in 1980 was ${avgTime}, and your average wait time in 2020 was ${avgTime2}.`)
+        }
+    }
+    if (hasSwitchedYear === 0 && currentYear === 2020) {
+        if (timer <= 0.1){
+            fishingStatus = 0
+            totalTime = 60
+            hasSwitchedYear = 1
+            reset()
+        }
+    }
+
+    currentYearDisplay.textContent = `Current Year: ${currentYear}`
+    totalFishText.textContent = `Total Fish for 1980: ${totalFish}`
+    totalFish2Text.textContent = `Total Fish for 2020: ${totalFish2}`
+    totalTimeDisplay.textContent = `Time Left: ${Math.floor(totalTime)}`
 }, 10);
 
 
 grayBar.setAttribute('style', `width: ${grayPosition}%`)
 
 function reset(){
-    fishingStatus = 0
     greenBar.style.display = 'none'
     grayBar.style.display = 'none'
     fishBar.style.display = 'none'
